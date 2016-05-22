4974fdfc01c579b5b7f6c1a5de9ee58ba60901c9
TianxiongWang <nickwang57@gmail.com>
2016-05-23 00:27:04 +0800

    get timer working

--- index.js
+++ index.js
@@ -2,6 +2,21 @@ var ExchangeRateClient = require('./ex_rate_client');
 
 var client = new ExchangeRateClient('USD', 'HKD');
 
-client.get_data(function(err, body) {
-    console.log(body);
+var results = [];
+var errors = 0;
+var timer = setInterval(function() {
+    if (results.length > 5) {
+        console.log(results);
+        clearInterval(timer);
+    }
+    client.get_data(function(err, body) {
+        if (err) {
+            errors++;
+            setTimeout(console.log("error"), 200);
+        }
+        else {
+            results.push(body);
+        }
+    });
 });
+    
--- test.js
+++ test.js
@@ -1,9 +1,9 @@
 var assert = require("assert");
 var ExchangeRateClient = require("./ex_rate_client");
 
-suite('ExchangeRateClient', function() { 
-    suite('#get_data', function() {
-        test('Api call success', function() {
+describe('ExchangeRateClient', function() { 
+    describe('#get_data', function() {
+        it('Api call success', function() {
             var client = new ExchangeRateClient('HKD', 'USD');
             client.get_data(function(err, body) {
                 assert.equal('HKD', body.from); 
