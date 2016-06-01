24c3d58870ce13127473d4e211cfc27ce0748763
TianxiongWang <nickwang57@gmail.com>
2016-06-01 19:36:31 -0400

    rate client

--- ex_rate_client.js
+++ ex_rate_client.js
@@ -1,28 +1,23 @@
-var request = require('request');
+var Promise = require("bluebird");
+var request = Promise.promisify(require('request'));
 
-var ex_rate_client = function (from, to) {
-    var url = 'http://api.fixer.io/latest?' + 'base=' + from + '&symbols=' + to;
-    this.get_data = function (callback) {
-        request(url, function (error, response, body) {
-            if (!error && response.statusCode == 200) {
-                try {
-                    body = JSON.parse(body);
-                    var res = {
-                        "from": from,
-                        "to": to,
-                        "created_at": Date.now(),
-                        "rate": Number((body.rates[to]).toFixed(2)).toString()
-                    };
-                    callback(error, res);
-                }
-                catch (err) {
-                    callback("Invalid JSON response", body);
-                }
-            }
-            else if (error) {
-                console.log('error: ' + error);
-            }
-        });
+var parse_data = function(from, to, res) {
+    return {
+        "from": from,
+        "to": to,
+        "created_at": Date.now(),
+        "rate": Number((res.rates[to]).toFixed(2)).toString()
     };
-}; 
-module.exports = ex_rate_client;
+};
+
+var get_data = function (from, to) {
+    return request('http://api.fixer.io/latest?' + 'base=' + from + '&symbols=' + to)
+        .then(function(response) {
+            return JSON.parse(response.body);
+        })
+        .then(parse_data.bind(null, from, to))
+        .catch(function(e) {
+            console.log("erorr");
+        });
+};
+exports.get_data = get_data;
